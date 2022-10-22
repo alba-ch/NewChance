23060636928441d4ad7106f7db4f7f6057ccb358
jhwan <jhwan818@gmail.com>
2022-10-22 08:49:31 +0200

    P3 RecyclerView

---
+++ app/src/main/java/jhwan/com/hackforgood/Adapter/P3ViewHolder.java
@@ -0,0 +1,56 @@
+package jhwan.com.hackforgood.Adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import jhwan.com.hackforgood.R;
+
+public class P3ViewHolder extends RecyclerView.ViewHolder {
+    private TextView title;
+    private TextView category;
+    private TextView message;
+    private TextView commentCount;
+
+    public P3ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.title = itemView.findViewById(R.id.p3_item_title);
+        this.category = itemView.findViewById(R.id.p3_item_category);
+        this.message = itemView.findViewById(R.id.p3_item_content);
+        this.commentCount = itemView.findViewById(R.id.p3_text_commentcount);
+    }
+
+    public TextView getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title.setText(title);
+    }
+
+    public TextView getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category.setText(category);
+    }
+
+    public TextView getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message.setText(message);
+    }
+
+    public TextView getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(String commentCount) {
+        this.commentCount.setText(commentCount);
+    }
+}
