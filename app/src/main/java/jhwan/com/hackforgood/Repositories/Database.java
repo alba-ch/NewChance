2a7e1f05850e4dcb8d2547de95e261a85d101425
albarransara <salbarranb2002@gmail.com>
2022-10-22 08:20:20 +0200

    arreglado pproblema firebase

--- app/src/main/java/jhwan/com/hackforgood/Repositories/Database.java
+++ app/src/main/java/jhwan/com/hackforgood/Repositories/Database.java
@@ -1,4 +1,13 @@
 package jhwan.com.hackforgood.Repositories;
 
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+/*
+This class will allow us to add and read information from our diferent
+database collections.
+ */
 public class Database {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 }
